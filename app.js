cd177614e3b99b19cb6f50bf53a651bbc6a501ec
Emil Dolorfino <edolorfino24@gmail.com>
2020-05-22 02:12:49 -0400

    Passing data finished

---
+++ app.js
@@ -0,0 +1,37 @@
+//@ts-check
+
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.get("/", (req, res) => {
+
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    let today = new Date().toLocaleDateString("en-US", options);
+
+    res.render("list", {
+        kindOfDay: today,
+        newListItems: items
+    });
+});
+
+app.post("/", (req, res) => {
+
+    items.push(req.body.newItem);
+    res.redirect("/");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
